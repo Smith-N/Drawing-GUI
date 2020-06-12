f8f7298c1837edb91502cf6a3de5e11f2c8dd717
Smith-N <43383992+Smith-N@users.noreply.github.com>
2020-06-12 15:02:07 -0400

    Add files via upload

---
+++ DrawingGUI/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace DrawingGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Location = new System.Drawing.Point(18, 109);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(753, 359);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ellipse",
+            "Circle",
+            "Square",
+            "Rectangle"});
+            this.comboBox1.Location = new System.Drawing.Point(573, 49);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 27);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(610, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Shape";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 16F);
+            this.label2.Location = new System.Drawing.Point(311, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Drawing GUI";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Black",
+            "Blue",
+            "Purple",
+            "Green",
+            "Red",
+            "Orange",
+            "Yellow"});
+            this.comboBox2.Location = new System.Drawing.Point(55, 49);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(127, 27);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(86, 24);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Color";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(18, 470);
+            this.hScrollBar1.Maximum = 40;
+            this.hScrollBar1.Minimum = 5;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(753, 17);
+            this.hScrollBar1.TabIndex = 6;
+            this.hScrollBar1.Value = 5;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(-1, 109);
+            this.vScrollBar1.Maximum = 40;
+            this.vScrollBar1.Minimum = 5;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 359);
+            this.vScrollBar1.TabIndex = 7;
+            this.vScrollBar1.Value = 5;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 488);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Drawing GUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+    }
+}
+
---
+++ DrawingGUI/Form1.cs
@@ -0,0 +1,129 @@
+﻿/* Nick Smith
+ * February 20, 2019
+ * Drawing GUI
+ * The program allows for the user to select from a list of shapes (Ellipse, Circle, Rectangle, Square), and a list of colors.
+ * The horizontal scroll bar affects the height of the shape, and the vertical scroll bar affects the width of the shape. 
+ * The shape dynamically draws when its width or height is changed, and changes color when a new color is selected. 
+ * I Was not able to get a check box or get the shape to move in the panel.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingGUI
+{
+    public partial class Form1 : Form
+    {
+        int height = 5; // default height set to 5
+        int width = 5; // default width set to 5
+        string shape; // shape variable
+        string color; // color variable
+        Graphics g;
+        Pen myPen = new Pen(Color.White); // pen set to white so that if shape is selected first, the program does not crash
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            shape = comboBox1.GetItemText(comboBox1.SelectedItem); // gets shape from combo box
+            
+            //g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            shape = comboBox1.GetItemText(comboBox1.SelectedItem);
+            
+            // if statements to draw selected shape
+            if (shape.Equals("Ellipse"))
+            {
+                g.DrawEllipse(myPen, 1, 1, height * 20, width * 10);
+            }
+            if (shape.Equals("Rectangle"))
+            {
+                g.DrawRectangle(myPen, 1, 1, height * 20, width * 10);
+            }
+            if (shape.Equals("Circle"))
+            {
+                g.DrawEllipse(myPen, 1, 1, height * 10, width * 10);
+            }
+            if (shape.Equals("Square"))
+            {
+                g.DrawRectangle(myPen, 1, 1, height * 10, width * 10);
+            } 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            color = comboBox2.GetItemText(comboBox2.SelectedItem); // gets color from combo box
+
+            if (color.Equals("Black"))
+            {
+                myPen = new Pen(Color.Black);
+            }
+            if (color.Equals("Blue"))
+            {
+                myPen = new Pen(Color.Blue);
+            }
+            if (color.Equals("Red"))
+            {
+                myPen = new Pen(Color.Red);
+            }
+            if (color.Equals("Purple"))
+            {
+                myPen = new Pen(Color.Purple);
+            }
+            if (color.Equals("Yellow"))
+            {
+                myPen = new Pen(Color.Yellow);
+            }
+            if (color.Equals("Green"))
+            {
+                myPen = new Pen(Color.Green);
+            }
+            if (color.Equals("Orange"))
+            {
+                myPen = new Pen(Color.Orange);
+            }
+            if (color.Equals("White"))
+            {
+                myPen = new Pen(Color.White);
+            }
+            comboBox1_SelectedIndexChanged(null, null); // redraws shape with new color
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            height = hScrollBar1.Value;
+            comboBox1_SelectedIndexChanged(null, null); //redraws shape with new height
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            width = vScrollBar1.Value;
+            comboBox1_SelectedIndexChanged(null, null); //redraws shape with new width
+        }
+    }
+}
